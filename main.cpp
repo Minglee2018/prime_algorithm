f312db45abd90b9898b06ee542d1fc905932a94f
Đoàn Mạnh Duy <hvktqs2014yb@gmail.com>
2019-02-13 21:58:49 +0700

    update code

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void Minglee()
+void snt1()
 {
 
   int N = 1000;
   bool check[N + 1];
-  // Kh?i t?o t?t c? các s? [2...N] d?u là s? nguyên t?
+  // Kh?i t?o t?t c? cÃ¡c s? [2...N] d?u lÃ  s? nguyÃªn t?
   for (int i = 2; i <= N; i++) {
     check[i] = true;
   }
 
-  // Thu?t toán sàng nguyên t?
-  // N?u m?t s? là s? nguyên t?, thì t?t c? các b?i c?a nó không ph?i s? nguyên t?
+  // Thu?t toÃ¡n sÃ ng nguyÃªn t?
+  // N?u m?t s? lÃ  s? nguyÃªn t?, thÃ¬ t?t c? cÃ¡c b?i c?a nÃ³ khÃ´ng ph?i s? nguyÃªn t?
   for (int i = 2; i <= N; i++) {
     if (check[i] == true) {
       for (int j = 2 * i; j <= N; j += i) {
@@ -20,12 +20,36 @@ void Minglee()
       }
     }
   }
-  // In ra các s? là s? nguyên t?
+  // In ra cÃ¡c s? lÃ  s? nguyÃªn t?
   for (int i = 2; i <= N; i++) {
     if (check[i] == true) {
       printf("%d ", i);
     }
   }
+}
+
+// tá»‘i Æ°u thuáº­t toÃ¡n. 
+void snt2(int x)
+{
+    ip[1] = 1;
+    int px = sqrt(x);
+    for (int i=2; i<=px; i++)
+        if (ip[i] == 0)
+        {
+            int j = i*i;
+            ip[j] = 1;
+            while (j <= x)
+            {
+                j = j + i;
+                ip[j] = 1;
+            }
+        }
+    for (int i=1; i<=x; i++)
+        if (ip[i] == 0)
+        {
+            cnt++;
+            pr[cnt] = i;
+        }
 }
 
 int main(int argc, char* argv[]) {
@@ -35,7 +59,9 @@ int main(int argc, char* argv[]) {
     if (argc > 2) {
         assert(freopen(argv[2], "wb", stdout));
     }
-    Minglee();
+    snt1();
+    cout <<endl;
+    snt2();
     cerr << "\nTime elapsed: " << 1000 * clock() / CLOCKS_PER_SEC << "ms\n";
     return 0;
 }
